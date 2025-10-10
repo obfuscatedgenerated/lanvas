8dc06f01c689fafc2281ed911dd49aeb9b9bc655
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-10-10 19:45:27 +0100

    PLEASE PLEASE

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
-export {default} from "next-auth/middleware"
+import {NextRequest, NextResponse} from "next/server";
 
-export const config = {
-    matcher: [
-        "/",
-        "/admin",
-        "/((?!api|_next/static|favicon.ico|opengraph-image).*)",
-    ]
+import {withAuth} from "next-auth/middleware";
+
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname === "/opengraph-image" || request.nextUrl.pathname === "/icon.ico") {
+        return NextResponse.next()
+    }
+
+    //@ts-expect-error seems to be the only way it works, and this should be a valid signature, just it doesnt have "nextauth" as a field?
+    return withAuth(request);
 }
