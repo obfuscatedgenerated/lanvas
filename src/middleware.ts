528afd2d2b7b45f5282270db727a3d37c17ae88c
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-10-10 19:36:21 +0100

    PLEASE

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,9 @@
-import { withAuth } from "next-auth/middleware";
-import {handler} from "@/auth";
-
-export default withAuth(handler);
+export {default} from "next-auth/middleware"
 
 export const config = {
     matcher: [
         "/",
         "/admin",
-        // exclude api routes, public, and special paths explicitly
         "/((?!api|_next/static|favicon.ico|opengraph-image).*)",
-    ],
-};
+    ]
+}
