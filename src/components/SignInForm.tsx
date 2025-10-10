b978da753d1867ad38c825d80b4568fd17935fa3
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-10-10 18:00:26 +0100

    privacy policy

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -9,7 +9,7 @@ export default function SignInForm() {
     const error = searchParams.get("error");
 
     return (
-        <div className="bg-neutral-800 rounded-2xl p-5 flex flex-col gap-4">
+        <div className="bg-neutral-800 rounded-2xl p-5 flex flex-col gap-4 w-9/10 max-w-9/10 sm:w-lg sm:max-w-lg">
             {error && (
                 <div className="text-red-500">
                     Sign-in failed.
@@ -29,9 +29,26 @@ export default function SignInForm() {
                     Privacy Policy
                 </summary>
 
-                <p>
-                    TODO
-                </p>
+                <div className="flex flex-col gap-3 mt-2 max-h-75 pr-3 overflow-y-scroll">
+                    <p>Only the minimum scopes required to operate the service are collected:</p>
+
+                    <ul className="list-disc">
+                        <li>
+                            <strong>Access your username, avatar and banner (<code>identify</code>)</strong> to get your unique Discord ID, username, and avatar. This information is used to show who placed which pixel.
+                        </li>
+                        <li>
+                            <strong>Know what servers you&apos;re in (<code>guilds</code>)</strong> to verify that you are a member of the required Discord server to gain access. This check is performed when you log in and <strong>your list of servers is never stored</strong>.
+                        </li>
+                    </ul>
+
+                    <p>At the end of the event, all pixels will be anonymised and all user details on record will be erased.</p>
+
+                    <p>If your account is banned from the service, your user ID will be stored indefinitely to prevent access to the service. For private, internal records, your username as it was at the time of the ban may be stored.</p>
+
+                    <p>To facilitate live previews, the canvas image data may be publicly accessible in certain circumstances.</p>
+
+                    <p>By using the service, you acknowledge the above. You also acknowledge the base <a target="_blank" rel="noreferrer noopener" className="text-blue-500" href="https://ollieg.codes/privacy">ollieg.codes Privacy Policy</a>.</p>
+                </div>
             </details>
         </div>
     );
