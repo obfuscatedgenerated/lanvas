6ff8fc05d51c2be0cfb5b6f5b5508600c0af4643
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-10-10 13:00:06 +0100

    load pixels from database on start

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import "dotenv/config";
 
-import { createServer } from "node:http";
+import {createServer} from "node:http";
 import next from "next";
 
-import { Server } from "socket.io";
+import {Server} from "socket.io";
 
-import { getToken } from "next-auth/jwt";
-import { parse as parse_cookies } from "cookie";
+import {getToken} from "next-auth/jwt";
+import {parse as parse_cookies} from "cookie";
+
+import {Pool} from "pg";
 
 const dev = process.env.NODE_ENV !== "production";
 
@@ -16,11 +18,19 @@ if (!NEXTAUTH_SECRET) {
     throw new Error("Missing NEXTAUTH_SECRET");
 }
 
+const pool = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT ? parseInt(process.env.PGPORT, 10) : 5432,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+});
+
 const hostname = process.argv[2] || "localhost";
 const port = parseInt(process.argv[3], 10) || 3000;
 
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({dev, hostname, port});
 const handler = app.getRequestHandler();
 
 const GRID_WIDTH = process.env.NEXT_PUBLIC_GRID_WIDTH ? parseInt(process.env.NEXT_PUBLIC_GRID_WIDTH) : 100;
@@ -29,22 +39,46 @@ const PIXEL_TIMEOUT_MS = process.env.PIXEL_TIMEOUT_MS ? parseInt(process.env.PIX
 
 console.log(`Grid size: ${GRID_WIDTH}x${GRID_HEIGHT}`);
 
-// basic test in memory, will switch to database once sockets work
-let grid_data = Array.from({ length: GRID_HEIGHT }, () =>
+// in memory caches with default empty values
+const grid_data = Array.from({length: GRID_HEIGHT}, () =>
     Array(GRID_WIDTH).fill("#FFFFFF")
 );
 
-let author_data = Array.from({ length: GRID_HEIGHT }, () =>
+const author_data = Array.from({length: GRID_HEIGHT}, () =>
     Array(GRID_WIDTH).fill(null)
 );
 
 // TODO: could reduce redundancy further by storing user ids only in author_data and having a separate user map
 
-let timeouts = {};
+const timeouts = {};
 
 const banned_user_ids = [];
 
-app.prepare().then(() => {
+const main = async () => {
+    await app.prepare();
+
+    // load existing pixels from database
+    const pixels = await pool.query("SELECT x, y, color, author_id, author.username, author.avatar_url FROM pixels JOIN user_details AS author ON pixels.author_id = author.user_id");
+
+    let loaded_pixel_count = 0;
+    for (const row of pixels.rows) {
+        const {x, y, color, author_id, username, avatar_url} = row;
+
+        // load each pixel into the in-memory grids
+        if (x >= 0 && x < GRID_WIDTH && y >= 0 && y < GRID_HEIGHT) {
+            grid_data[y][x] = color;
+            author_data[y][x] = {
+                user_id: author_id,
+                name: username,
+                avatar_url,
+            };
+
+            loaded_pixel_count++;
+        }
+    }
+
+    console.log(`Loaded ${loaded_pixel_count} pixels from database.`);
+
     const http_server = createServer(handler);
 
     const io = new Server(http_server);
@@ -81,7 +115,7 @@ app.prepare().then(() => {
         }
 
         console.log(`Client connected: ${socket.id}, user ${socket.user.name} (id: ${socket.user.sub})`);
-        
+
         // send full grid to client when requested
         socket.on("request_full_grid", () => {
             console.log(`Full grid requested by: ${socket.id}`);
@@ -97,7 +131,7 @@ app.prepare().then(() => {
         // handle pixel updates from clients
         socket.on("pixel_update", (payload) => {
             try {
-                const { x, y, color } = payload;
+                const {x, y, color} = payload;
                 //console.log(`Received pixel_update from ${socket.id}:`, payload);
 
                 // basic validation of incoming data
@@ -114,7 +148,7 @@ app.prepare().then(() => {
                 // check if user is banned
                 const user_id = socket.user.sub;
                 if (banned_user_ids.includes(user_id)) {
-                    socket.emit("pixel_update_rejected", { reason: "banned" });
+                    socket.emit("pixel_update_rejected", {reason: "banned"});
                     return;
                 }
 
@@ -123,7 +157,7 @@ app.prepare().then(() => {
                 if (timeouts[user_id] && timeouts[user_id] > current_time) {
                     // user is still in timeout period
                     const wait_time = Math.ceil((timeouts[user_id] - current_time) / 1000);
-                    socket.emit("pixel_update_rejected", { reason: "timeout", wait_time });
+                    socket.emit("pixel_update_rejected", {reason: "timeout", wait_time});
                     return;
                 }
 
@@ -141,7 +175,7 @@ app.prepare().then(() => {
                 timeouts[user_id] = current_time + PIXEL_TIMEOUT_MS;
 
                 // broadcast the pixel update to all connected clients
-                io.emit("pixel_update", { x, y, color, author });
+                io.emit("pixel_update", {x, y, color, author});
             } catch (error) {
                 console.error("Invalid pixel_update payload", error);
             }
@@ -160,4 +194,6 @@ app.prepare().then(() => {
         .listen(port, () => {
             console.log(`> Ready on http://${hostname}:${port}`);
         });
-});
+}
+
+main();
