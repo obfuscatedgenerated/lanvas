00aeea98ddf1e59fac47705d7a13c0efea9a57a2
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-11-29 21:08:43 +0000

    add lan number to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import {Suspense} from "react";
 import LoginStatus from "@/components/LoginStatus";
 
 const Header = () => {
+    const lan_number = process.env.NEXT_PUBLIC_LAN_NUMBER || "";
+
     return (
         <header className="font-sans p-2 bg-orange-700 text-white flex items-center justify-between">
-            <h1 className="text-2xl font-bold font-doodle">LANvas</h1>
+            <h1 className="text-2xl font-bold font-doodle">LANvas {lan_number}</h1>
 
             <Suspense fallback="Loading...">
                 <LoginStatus />
