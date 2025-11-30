851c5cfb60097d58fd8ab8ed25d158493b27e222
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-11-30 00:34:04 +0000

    make clicking title return home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import {Suspense} from "react";
 import LoginStatus from "@/components/LoginStatus";
+import Link from "next/link";
 
 const Header = () => {
     const lan_number = process.env.NEXT_PUBLIC_LAN_NUMBER || "";
 
     return (
         <header className="font-sans p-2 bg-orange-700 text-white flex items-center justify-between">
-            <h1 className="text-xl sm:text-2xl font-bold font-doodle">LANvas {lan_number}</h1>
+            <Link href="/" title="Click to return to home">
+                <h1 className="text-xl sm:text-2xl font-bold font-doodle">LANvas {lan_number}</h1>
+            </Link>
 
             <Suspense fallback="Loading...">
                 <LoginStatus />
