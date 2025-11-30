d2dc99644b4b4f161f007b9c14539515b7842590
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-11-30 03:05:46 +0000

    improve admin message marquee

--- src/components/FloatingAdminMessage.tsx
+++ src/components/FloatingAdminMessage.tsx
@@ -6,11 +6,17 @@ import { socket } from "@/socket";
 const FloatingAdminMessage = () => {
     const [message, setMessage] = useState<string | null>(null);
     const [persistent, setPersistent] = useState(false);
+    const [key, setKey] = useState(0);
 
     // setup socket listener
     useEffect(() => {
         socket.on("admin_message", ({ message, persist }: { message: string; persist: boolean }) => {
             console.log("Received admin message:", message, "persist:", persist);
+
+            if (!persist) {
+                // change key to trigger re-mount for animation (using message as key doesn't work if same message is sent twice)
+                setKey(prevKey => prevKey + 1);
+            }
 
             setMessage(message);
             setPersistent(persist);
@@ -25,7 +31,7 @@ const FloatingAdminMessage = () => {
         <div className="relative w-full h-0 pointer-events-none select-none z-9999 pt-2 text-orange-600 drop-shadow-md font-sans text-xl flex">
             {persistent
                 ? <span className="text-center w-full">{message}</span>
-                : <span key={message} className="inline-block animate-scroll-from-right">{message}</span>
+                : <span key={key} className="inline-block animate-scroll-from-right text-nowrap">{message}</span>
             }
         </div>
     );
