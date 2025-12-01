5d9bc796c7d3ebe905ea0e230b4507afacd485a2
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-12-01 02:06:40 +0000

    explicit stat ordering

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -15,6 +15,11 @@ const known_stat_labels: { [key: string]: string } = {
     connected_unique_users: "Connected users",
 };
 
+const stats_order = [
+    "total_pixels_placed",
+    "connected_unique_users",
+];
+
 const StatsList = ({ stats }: { stats: StatsData }) => (
     <div className="grid grid-cols-[minmax(0,1fr)_auto] max-w-9/10 gap-x-6 gap-y-2 text-xl sm:text-2xl items-center mb-16">
         {Object.entries(stats).map(([key, value]) => (
@@ -32,7 +37,20 @@ export default function StatsPage() {
     // register socket listener
     useEffect(() => {
         socket.on("stats", (data) => {
-            setStats(data);
+            // sort stats according to predefined order, then whatever order recieved for unknown stats
+            const sorted_stats: Partial<StatsData> = {};
+            for (const key of stats_order) {
+                if (key in data) {
+                    sorted_stats[key] = data[key];
+                }
+            }
+            for (const [key, value] of Object.entries(data)) {
+                if (!(key in sorted_stats)) {
+                    // @ts-expect-error guaranteed to be correct type
+                    sorted_stats[key] = value;
+                }
+            }
+            setStats(sorted_stats as StatsData);
         });
 
         // join stats room and request stats on mount
