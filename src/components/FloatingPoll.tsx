66edc6cc92ce905bd7a574f259bf6c36a52757a4
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-12-04 17:17:46 +0000

    bring poll ux in line with comment control

--- src/components/FloatingPoll.tsx
+++ src/components/FloatingPoll.tsx
@@ -74,7 +74,7 @@ const FloatingPoll = () => {
     const hidden = poll_state === PollState.HIDDEN || user_hiding;
 
     return (
-        <div className={`z-9999 fixed right-[50vw] translate-x-[50%] sm:translate-x-0 top-20 sm:right-10 min-w-64 max-w-9/10 sm:max-w-100 bg-neutral-600 border border-neutral-500 rounded shadow-lg p-4 transition-opacity duration-500 ${hidden ? "opacity-0 pointer-events-none" : "opacity-100"}`}>
+        <div className={`z-9999 font-sans fixed right-[50vw] translate-x-[50%] sm:translate-x-0 top-20 sm:right-10 min-w-64 w-full sm:w-fit max-w-9/10 sm:max-w-100 bg-neutral-600/75 backdrop-blur-sm border border-neutral-500/75 rounded shadow-lg p-4 transition-opacity duration-500 ${hidden ? "opacity-0 pointer-events-none" : "opacity-100"}`}>
             <div className="w-full flex items-start justify-between mb-2 gap-2">
                 {question && <h3 className="text-lg font-semibold break-words max-w-[92.5%]">{question}</h3>}
 
@@ -83,10 +83,11 @@ const FloatingPoll = () => {
                 </button>
             </div>
 
+            <div>
             {options && options.map((option, index) => (
                 <button
                     key={index}
-                    className={`${chosen_option_index === index ? "outline-2 outline-orange-200/80" : ""} ${winners && winners.includes(option) ? "shadow-[0_0_20px_5px_rgba(234,179,8,0.6)] !bg-yellow-200 text-black" : ""} flex justify-between w-full mb-2 break-words px-3 py-2 bg-orange-700 hover:bg-orange-600 transition-all rounded disabled:bg-neutral-400 cursor-pointer disabled:cursor-not-allowed`}
+                    className={`${chosen_option_index === index ? "outline-2 outline-orange-200/80" : ""} ${winners && winners.includes(option) ? "shadow-[0_0_20px_5px_rgba(234,179,8,0.6)] !bg-yellow-200 text-black" : ""} flex justify-between gap-2 sm:gap-4 w-full mb-2 break-words px-3 py-2 bg-orange-700 hover:bg-orange-600 transition-all rounded disabled:bg-neutral-400 cursor-pointer disabled:cursor-not-allowed`}
                     onClick={() => {
                         if (poll_state === PollState.ACTIVE) {
                             socket.emit("poll_vote", index);
@@ -96,15 +97,19 @@ const FloatingPoll = () => {
                     disabled={poll_state !== PollState.ACTIVE}
                     title={poll_state === PollState.ACTIVE ? "Click to vote for this option" : ""}
                 >
-                    {option}
+                    <span className="break-all text-left flex-1">
+                        {option}
+                    </span>
+
                     {counts && counts[index] !== undefined && (
-                        <div className="flex gap-1">
+                        <div className="flex gap-1 min-w-30 text-nowrap shrink-0 items-center justify-end">
                             <span className="text-sm">{counts[index]} vote{counts[index] !== 1 ? "s" : ""}</span>
                             <span className="text-sm">({total_votes > 0 ? ((counts[index] / total_votes) * 100).toFixed(1) : "0.0"}%)</span>
                         </div>
                     )}
                 </button>
             ))}
+            </div>
         </div>
     );
 };
